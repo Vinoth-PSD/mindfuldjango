848295bdda23f11ba4f442e9daaa5479525f5824
Vinoth-PSD <156505851+Vinoth-PSD@users.noreply.github.com>
2025-07-22 18:42:14 +0530

    Serializersupdateforworkinghours

--- serviceapp/serializers.py
+++ serviceapp/serializers.py
@@ -128,8 +128,9 @@ class SalonDetailsSerializer(serializers.ModelSerializer):
         default=["8:00 AM,9:00 AM,10:00 AM,11:00 AM,12:00 PM,1:00 PM,2:00 PM,3:00 PM,4:00 PM,5:00 PM,6:00 PM,7:00 PM "]
     )
     image_url = serializers.ImageField(required=False)  
+    working_hours= serializers.CharField(required=False) 
 
-    
+
 
     print(saloon_address , saloon_location)
     class Meta:
